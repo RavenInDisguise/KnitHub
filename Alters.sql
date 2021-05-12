4ca73fa57e6f91a890fac7e74d921b4642274914
MauAguero12 <mam.121002@gmail.com>
2021-05-11 22:23:10 -0600

    inicio sp anidado

--- Alters.sql
+++ Alters.sql
@@ -2,3 +2,18 @@ ALTER TABLE Patterns
 ADD creationDate DATETIME;
 ALTER TABLE Projects
 ADD creationDate DATETIME;
+
+ALTER TABLE PriceValues
+ADD CurrencySymbol VARCHAR(6) NOT NULL;
+
+ALTER TABLE PaymentsAttempts
+MODIFY Username NVARCHAR(65) NOT NULL;
+
+ALTER TABLE PaymentsAttempts
+RENAME TO PaymentAttempts;
+
+ALTER TABLE PaymentAttempts 
+RENAME COLUMN PaymentAttemptsId TO PaymentAttemptId;
+
+ALTER TABLE Transactions 
+RENAME COLUMN PaymentAttemptsId TO PaymentAttemptId;
