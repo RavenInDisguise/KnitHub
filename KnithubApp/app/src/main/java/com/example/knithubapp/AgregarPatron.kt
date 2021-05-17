c4bc82cccd8530f242b9930dc60d33e8aae4ad1e
daae02 <65504346+daae02@users.noreply.github.com>
2021-05-16 22:47:57 -0600

    Detalles Finales App

--- KnithubApp/app/src/main/java/com/example/knithubapp/AgregarPatron.kt
+++ KnithubApp/app/src/main/java/com/example/knithubapp/AgregarPatron.kt
@@ -1,8 +1,9 @@
 package com.example.knithubapp
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.support.v7.app.AlertDialog
 import android.widget.Button
 import android.widget.EditText
 import kotlinx.coroutines.GlobalScope
@@ -13,28 +14,46 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
 
 class AgregarPatron : AppCompatActivity() {
+    var change = false
+    var result = "Patrón creado con éxito"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_agregar_patron)
-        val agregarPatron = findViewById<Button>(R.id.agregarPatron)
-        agregarPatron.setOnClickListener {
+        val agregarDatosPatron = findViewById<Button>(R.id.agregarDatosPatron)
+        agregarDatosPatron.setOnClickListener {
             val macEntry = findViewById<EditText>(R.id.entryMac)
             val nameEntry = findViewById<EditText>(R.id.entryName)
             val lastNameEntry = findViewById<EditText>(R.id.entryLastName)
             val patName = findViewById<EditText>(R.id.entryPatternName)
             val patCat = findViewById<EditText>(R.id.entryCategory)
             val patDesc = findViewById<EditText>(R.id.entryDescripcion)
-            GlobalScope.async {registrarPatron(macEntry.text.toString(),nameEntry.text.toString(),lastNameEntry.text.toString(),patName.text.toString(),patCat.toString(),patDesc.toString())}
+            GlobalScope.async {registrarPatron(macEntry.text.toString(),nameEntry.text.toString(),lastNameEntry.text.toString(),patName.text.toString(),patCat.text.toString(),patDesc.text.toString())}
+            while(!change){}
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setCancelable(false).setMessage(result).setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, id -> finish()  })
+            val alert = dialogBuilder.create()
+            alert.setTitle("Resultado")
+            alert.show()
         }
+
     }
 
     private fun registrarPatron(mac: String, name: String, lName: String, patName: String,patCat: String ,patDesc: String) {
-        val url = "http://192.168.8.102:8080/compraPatrones"
+        val url = "http://192.168.8.102:8080/knithub/add/patterns"
         val client = OkHttpClient()
         val json = "application/json; charset=utf-8".toMediaType()
         val jsonString = ("{\"macaddress\":\"$mac\",\"userName\":\"$name\",\"lastName\":\"$lName\",\"patternName\":\"$patName\",\"categoryName\":\"$patCat\",\"description\":\"$patDesc\"}")
         val body = jsonString.toRequestBody(json)
         val request = Request.Builder().url(url).post(body).build()
-        val  response = client . newCall (request).execute()
-    }
+        try {
+            var response = client . newCall (request).execute()
+            if (!response.isSuccessful) {
+                result = "No se pudo agregar, intentelo más tarde"
+            }
+        }
+        catch (IOException : Exception){
+            result = "Ocurrió un problema"
+        }
+        change = true
+        }
 }
