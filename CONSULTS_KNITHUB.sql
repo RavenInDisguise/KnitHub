29e8f905ab501452bc59d408f8f13e42f35a0499
RavenInDisguise <monicaalfarop@estudiantec.cr>
2021-05-18 04:24:28 -0600

    Actualizacion de Consults

--- CONSULTS_KNITHUB.sql
+++ CONSULTS_KNITHUB.sql
@@ -1,20 +1,18 @@
 -- Una consulta que retorne un listado de los montos y personas, categorízados por año y mes de
 -- aquellos dineros que no se han podido cobrar, en el query debe poder verse las categorías, nombres
 -- y montos debidamente agrupados.
-
 SELECT 
-(SELECT CONCAT(Users.Name, ' ', Users.LastName) FROM Users) AS UserName,
-SUM(PaymentAttempts.Amount) AS NotPayedAmounts,
-MONTH(PaymentAttempts.PostTime) AS 'Month',
-MONTHNAME(PaymentAttempts.PostTime) AS 'MonthName',
-YEAR(PaymentAttempts.PostTime) AS 'Year'
+MONTH(PaymentAttempts.PostTime) AS 'Mes',
+YEAR(PaymentAttempts.PostTime) AS 'Anno',
+GROUP_CONCAT((SELECT CONCAT(Users.Name,' ',Users.LastName) FROM Users WHERE Users.UserId=PaymentAttempts.UserId LIMIT 1)) AS 'Lista de usuarios',
+SUM(PaymentAttempts.Amount) AS NotPayedAmounts
 FROM PaymentAttempts
 INNER JOIN Users ON PaymentAttempts.UserId=Users.UserId
-WHERE PaymentAttempts.PaymentStatusId!=(SELECT PaymentStatus.PaymentStatusId FROM PaymentStatus WHERE PaymentStatus.Name="Aceptado")
-AND PaymentAttempts.ReferenceNumber 
-NOT IN(SELECT Transactions.ReferenceNumber FROM Transactions)
-GROUP BY PostTime, Username 
-ORDER BY 'Month' ASC, 'Year' DESC; 
+WHERE PaymentAttempts.PaymentStatusId!=(SELECT (PaymentStatus.PaymentStatusId) FROM PaymentStatus WHERE PaymentStatus.Name="Aceptado")
+AND PaymentAttempts.ReferenceNumber!=(SELECT(Transactions.ReferenceNumber) FROM Transactions LIMIT 1)
+AND PaymentAttempts.UserId=Users.UserId
+GROUP BY MONTH(PostTime), YEAR(PostTime)
+ORDER BY 'Mes' ASC, 'Anno' DESC; 
 
 -- Una consulta que retorne el volumen de operaciones de uso del sistema por mes en un rango de
 -- fechas, clasificado entre bajo volumen, volumen medio y volumen alto
